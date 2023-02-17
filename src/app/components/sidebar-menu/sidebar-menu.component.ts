4713387ef420e6568162606a073b710fcc9eb3bd
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-17 12:26:48 +0100

    Create sidebar-menu component

---
+++ src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  templateUrl: './sidebar-menu.component.html',
+  styleUrls: ['./sidebar-menu.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class SidebarMenuComponent {
+
+}
