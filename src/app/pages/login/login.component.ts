99d96a51d13e364480c3b61606b6019551fb1eed
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-17 09:42:14 +0100

    Get user token

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpotifyService } from '@app/services/spotify.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
@@ -8,9 +9,22 @@ import { SpotifyService } from '@app/services/spotify.service';
   standalone: true,
   imports: [CommonModule],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   private spotifyService = inject(SpotifyService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.verifyTokenUrlCallback();
+  }
+
+  verifyTokenUrlCallback() {
+    const token = this.spotifyService.getTokenUrlCallback();
+    if(!!token){
+      this.spotifyService.setAccessToken(token);
+    }
+  }
+
 
   openLoginUrl() {
     window.location.href = this.spotifyService.getLoginUrl();
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import Spotify from 'spotify-web-api-js';
+import { User } from '@app/interfaces/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
+  spotifyApi: Spotify.SpotifyWebApiJs = null;
+  user: User;
   authEndpoint: string = `${environment.authEndpoint}?`;
   clientId: string = `client_id=${environment.clientId}&`;
   redirectUrl: string = `redirect_uri=${environment.redirectUrl}&`;
   scopes: string = `scope=${environment.scopes.join('%20')}&`;
   responseType: string = `response_type=token&show_dialog=true`;
 
+  constructor() {
+    this.spotifyApi = new Spotify();
+  }
+
   getLoginUrl(): string {
     return this.authEndpoint + this.clientId + this.redirectUrl + this.scopes + this.responseType;
+  }
+
+  getTokenUrlCallback(): string {
+    if (!window.location.hash)
+      return '';
+
+    const params = window.location.hash.substring(1).split('&');
+    return params[0].split('=')[1];
+  }
+
+  setAccessToken(token: string){
+    this.spotifyApi.setAccessToken(token);
   }
 }
