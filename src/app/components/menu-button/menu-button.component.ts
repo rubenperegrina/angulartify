c02921a291f7615f5a0bc2fc8e31b1e75c590296
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-17 12:45:00 +0100

    Create menu button component

---
+++ src/app/components/menu-button/menu-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-button',
+  templateUrl: './menu-button.component.html',
+  styleUrls: ['./menu-button.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class MenuButtonComponent {
+
+}
