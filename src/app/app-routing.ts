048ee81b7b4ce1117d150d5f5f78beaf126d02fb
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-17 12:21:02 +0100

    Refactor auth guard

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,6 +3,21 @@ import { Routes } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { SpotifyService } from './services/spotify.service';
 
+export const authGuard = () => {
+  const spotifyService = inject(SpotifyService);
+  const token = spotifyService.getAccessToken();
+  if (!token)
+    spotifyService.logOut();
+
+  return new Promise(async (res) => {
+    const userCreated = await spotifyService.initUser();
+    if (userCreated)
+      res(true);
+    else
+      res(spotifyService.logOut());
+  })
+}
+
 export const routes: Routes =
   [
     {
@@ -14,7 +29,7 @@ export const routes: Routes =
       path: 'home',
       loadComponent: () => import('./pages/home/home.component')
         .then(m => m.HomeComponent),
-        canActivate: [() => inject(SpotifyService).isLoggedIn()]
+      canActivate: [authGuard]
     },
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
