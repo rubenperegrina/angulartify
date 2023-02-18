86760644f1a1daa788e1f30c1ea40ebc9fa06d10
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-18 20:22:09 +0100

    Create top artist component

---
+++ src/app/components/top-artist/top-artist.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-top-artist',
+  templateUrl: './top-artist.component.html',
+  styleUrls: ['./top-artist.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class TopArtistComponent {
+
+}
