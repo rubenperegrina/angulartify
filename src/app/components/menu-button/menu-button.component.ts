d469599201250dc8036973d904c723a1ed7b5011
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-19 14:29:14 +0100

    Add playlist cover in sidebar

--- src/app/components/menu-button/menu-button.component.ts
+++ src/app/components/menu-button/menu-button.component.ts
@@ -1,18 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-menu-button',
   templateUrl: './menu-button.component.html',
   styleUrls: ['./menu-button.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
 })
 export class MenuButtonComponent {
   @Input() description = '';
-
+  @Input() imageUrl = '';
+  @Input() icon: IconDefinition = undefined;
   @Input() select = false;
-
   @Output() buttonClicked = new EventEmitter<void>();
 
   onClick() {
