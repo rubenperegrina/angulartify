5f340050290ebc4e6988616d124213d7199b0384
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-19 14:44:35 +0100

    Create library and search component

---
+++ src/app/pages/library/library.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-library',
+  templateUrl: './library.component.html',
+  styleUrls: ['./library.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class LibraryComponent {
+
+}
---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class SearchComponent {
+
+}
