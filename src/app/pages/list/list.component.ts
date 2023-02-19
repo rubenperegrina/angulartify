fa05c031a3a332fbf93a79125a41f3da00db81bc
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-19 10:02:27 +0100

    Create list component

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class ListComponent {
+
+}
